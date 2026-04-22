8f39a0de5e3285cb57054eb81bf2453542848a12
Lekkala Vishnuvardhanreddy <lekkalavishnu7675@gmail.com>
2026-04-22 10:33:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
 from flask import Flask, render_template, request
-import requests
+from weather import get_weather
 
 app = Flask(__name__)
 
-API_KEY = "6bbf47443d9949f7d14c0e35ea1c1580"
-
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
     weather = None
-    
+
     if request.method == "POST":
         city = request.form["city"]
-
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-        response = requests.get(url)
-        data = response.json()
-
-        if data["cod"] == 200:
-            weather = {
-                "city": city,
-                "temp": data["main"]["temp"],
-                "humidity": data["main"]["humidity"],
-                "desc": data["weather"][0]["description"]
-            }
+        weather = get_weather(city)
 
     return render_template("index.html", weather=weather)
 
