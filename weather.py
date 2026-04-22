25003ad75943e792211bc69d4548bb2667ef98c5
Lekkala Vishnuvardhanreddy <lekkalavishnu7675@gmail.com>
2026-04-22 10:34:00 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,22 +1,43 @@
 import requests
 
-api_key = "YOUR_API_KEY"
+API_KEY = "6bbf47443d9949f7d14c0e35ea1c1580"
 
-city = input("Enter city name: ")
+def get_weather(city):
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
-response = requests.get(url)
-data = response.json()
+    response = requests.get(url)
+    data = response.json()
 
-if data["cod"] == 200:
-    print("\n🌤 Weather Report")
-    print("-----------------------")
-    print("City:", data["name"])
-    print("Temperature:", data["main"]["temp"], "°C")
-    print("Feels Like:", data["main"]["feels_like"], "°C")
-    print("Humidity:", data["main"]["humidity"], "%")
-    print("Weather:", data["weather"][0]["description"])
-    print("Wind Speed:", data["wind"]["speed"], "m/s")
-else:
-    print("Error:", data["message"])
+    if data["cod"] != 200:
+        return None
+
+    condition = data["weather"][0]["main"].lower()
+
+    # Default
+    icon = ""
+    background = ""
+
+    if "clear" in condition:
+        icon = "☀"
+        background = "sunny"
+    elif "rain" in condition:
+        icon = "🌧"
+        background = "rain"
+    elif "cloud" in condition:
+        icon = "☁"
+        background = "cloud"
+    else:
+        icon = "🌙"
+        background = "night"
+
+    weather = {
+        "city": city,
+        "temperature": data["main"]["temp"],
+        "humidity": data["main"]["humidity"],
+        "description": data["weather"][0]["description"],
+        "icon": icon,
+        "background": background
+    }
+
+    return weather
